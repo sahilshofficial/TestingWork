46b97d6c4618550a6bbe2d7d9e9bb538e55859a5
Sahil Sharma <sahilshofficial@gmail.com>
2018-07-06 00:54:21 +0530

    forLoops Question

---
+++ ForLoopQues/src/Ques1.java
@@ -0,0 +1,24 @@
+
+public class Ques1 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=4;i++)
+		{
+			if(i==2||i==3)
+			{
+				System.out.print("$  $");
+			}
+			else
+			{
+			for(int j=1;j<=4;j++)
+			{
+				System.out.print("$");
+				
+			}
+			}
+			System.out.println("");
+		}
+	}
+
+}
---
+++ ForLoopQues/src/Ques2.java
@@ -0,0 +1,18 @@
+
+public class Ques2 {
+
+	public static void main(String[] args) {
+
+		
+		for(int l=65;l<=67;l++)
+		{
+			for(int k=65;k<=l;k++)
+			{
+				System.out.print((char)k+" ");
+			}
+			System.out.println();
+
+		}
+	}
+
+}
